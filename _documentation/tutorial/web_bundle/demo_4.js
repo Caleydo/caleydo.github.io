0fab7495e700fa8a4b9940fc5da1c428423b8904
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-11 16:06:37 -0400

    Rename + wrap in functions iframes still look good.

---
+++ _documentation/tutorial/web_bundle/demo_4.js
@@ -0,0 +1,28 @@
+  function demo_4($target) {
+    $target.append('<div class="visual">');
+    $target.append('<div class="toolbar">');
+    $target.append('<div class="visual">');
+    $target.append('<div class="toolbar">');
+
+    Caleydo.d3.parser.parseRemoteMatrix( // Download data
+        '/documentation/tutorial/web_bundle/data.csv'
+    ).then(function (matrix) {
+          var multiform = Caleydo.core.multiform;
+
+          function visualize(index) { // Make a function so we can have more than one
+            var visual = multiform.create(
+                matrix,
+                $target.find('.visual')[index],
+                {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
+            );
+            multiform.addSelectVisChooser( // Let user pick visualization
+                $target.find('.toolbar')[index],
+                visual
+            );
+          }
+
+          visualize(0);
+          visualize(1);
+        }
+    )
+  }
