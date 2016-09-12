b2109a59a167013a2d0c7ae783084839eaa0f35d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-11 22:57:44 -0400

    Group QUnit tests into (trivial) module

--- _documentation/tutorial/web_bundle/test_0.js
+++ _documentation/tutorial/web_bundle/test_0.js
@@ -1,5 +1,10 @@
+QUnit.module('demo_0',{
+  beforeEach: function() {
+    demo_0($('#qunit-fixture'));
+  }
+})
+
 QUnit.test( "table head?", function( assert ) {
-  demo_0($('#qunit-fixture'));
   var done = assert.async();
   window.setTimeout(function(){
     assert.ok( $('#qunit-fixture').html().includes('<th>ID</th><th>A</th>'));
@@ -8,7 +13,6 @@ QUnit.test( "table head?", function( assert ) {
 });
 
 QUnit.test( "table row?", function( assert ) {
-  demo_0($('#qunit-fixture'));
   var done = assert.async();
   window.setTimeout(function(){
     assert.ok( $('#qunit-fixture').html().includes('<th>1</th><td>0</td>'));
