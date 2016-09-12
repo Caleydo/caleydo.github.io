5d3c55032b5da1e84ab6240c45c19e236609f921
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-11 22:45:35 -0400

    Get more info about failure (Passes locally, as you would expect.)

--- test.rb
+++ test.rb
@@ -1,11 +1,19 @@
 require 'selenium-webdriver'
 
-# TODO: Use a real testing library
+# TODO: Use a real testing library?
 
 driver = Selenium::WebDriver.for(:firefox)
 driver.navigate.to('http://localhost:4000/documentation/tutorial/web_bundle/qunit.html?0')
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-wait.until { driver.find_element(css: '#qunit-testresult .failed') }
+
+begin
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  wait.until { driver.find_element(css: '#qunit-testresult .failed') }
+rescue Selenium::WebDriver::Error::TimeOutError
+  puts driver.page_source
+  puts driver.find_element(css: 'body').text
+  puts 'FAIL'
+  exit 1
+end
 
 puts driver.find_element(css: 'body').text
 
@@ -14,6 +22,6 @@ driver.quit # Is this necessary?
 if fail_count == '0'
   puts 'PASS'
 else
-  warn 'FAIL'
+  puts 'FAIL'
   exit 1
 end
