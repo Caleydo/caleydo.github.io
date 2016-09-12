f74dc2ec45bde9cd4282fc924d5f8ad9bcc79bbf
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-12 12:16:59 -0400

    Reorder definintions for clarity

--- _documentation/tutorial/web_bundle/qunit_support.js
+++ _documentation/tutorial/web_bundle/qunit_support.js
@@ -1,24 +1,3 @@
-function includes(haystack, needle) {
-  // String.prototype.includes exists in recent versions,
-  // but this can save you from one cryptic error if you are out of date.
-  return haystack.indexOf(needle) !== -1
-}
-
-function timeout_0(assert, assertion) {
-  var done = assert.async();
-  window.setTimeout(function(){
-    assertion(assert);
-    done();
-  }, 0);
-}
-
-function assert_includes(assert, needle) {
-  timeout_0(assert, function() {
-    var haystack = $('#qunit-fixture').html();
-    assert.ok(includes(haystack, needle), needle + ' not in ' + haystack);
-  });
-}
-
 function qunit_module(name) {
   QUnit.module(name,{
     beforeEach: function() {
@@ -31,4 +10,25 @@ function qunit_test_includes(name, needle) {
   QUnit.test(name, function(assert) {
     assert_includes(assert, needle);
   });
+}
+
+function assert_includes(assert, needle) {
+  timeout_0(assert, function() {
+    var haystack = $('#qunit-fixture').html();
+    assert.ok(includes(haystack, needle), needle + ' not in ' + haystack);
+  });
+}
+
+function timeout_0(assert, assertion) {
+  var done = assert.async();
+  window.setTimeout(function(){
+    assertion(assert);
+    done();
+  }, 0);
+}
+
+function includes(haystack, needle) {
+  // String.prototype.includes exists in recent versions,
+  // but this can save you from one cryptic error if you are out of date.
+  return haystack.indexOf(needle) !== -1
 }
