c827d9bcbddc35527565ea5b62c82ca645c91d97
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-14 10:56:28 -0400

    Give file loading 1 second to complete I would like a more elegant solution, but the promise is an internal and I do not have a good way of exposing it at higher levels.

--- _documentation/tutorial/web_bundle/qunit_support.js
+++ _documentation/tutorial/web_bundle/qunit_support.js
@@ -1,30 +1,31 @@
-function qunit_module(name) {
+function qunit_module(name, timeout) {
   QUnit.module(name,{
     beforeEach: function() {
       window[name]($('#qunit-fixture'));
+      this.timeout = timeout ? timeout : 0;
     }
   });
 }
 
 function qunit_test_includes(name, needle) {
   QUnit.test(name, function(assert) {
-    assert_includes(assert, needle);
+    assert_includes(assert, needle, this.timeout);
   });
 }
 
-function assert_includes(assert, needle) {
-  timeout_0(assert, function() {
+function assert_includes(assert, needle, timeout) {
+  assert_with_timeout(assert, timeout, function() {
     var haystack = $('#qunit-fixture').html();
     assert.ok(includes(haystack, needle), needle + ' not in ' + haystack);
   });
 }
 
-function timeout_0(assert, assertion) {
+function assert_with_timeout(assert, timeout, assertion) {
   var done = assert.async();
   window.setTimeout(function(){
     assertion(assert);
     done();
-  }, 0);
+  }, timeout);
 }
 
 function includes(haystack, needle) {
--- _documentation/tutorial/web_bundle/test_3.js
+++ _documentation/tutorial/web_bundle/test_3.js
@@ -1,5 +1,3 @@
-// I am getting sporadic errors here which I can't reproduce locally.
-
-// qunit_module('demo_3');
-// qunit_test_includes('heat map NW square?',
-//     '<rect fill="#ff5e5e" y="0" x="0" height="1" width="1"><title>10</title></rect>');
+qunit_module('demo_3', 1000); // Wait for 1 second to load external file.
+qunit_test_includes('heat map NW square?',
+    '<rect fill="#ff5e5e" y="0" x="0" height="1" width="1"><title>10</title></rect>');
