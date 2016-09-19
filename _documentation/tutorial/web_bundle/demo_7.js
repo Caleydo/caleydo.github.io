80e738fc48a011515e9a068e34cfbb32e12142f4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-19 19:57:39 -0400

    componetize

--- _documentation/tutorial/web_bundle/demo_7.js
+++ _documentation/tutorial/web_bundle/demo_7.js
@@ -1,14 +1,23 @@
 function demo_7() {
-
-  var demoApp = angular.module('demoApp', []);
-
-  demoApp.controller('DemoController', function DemoController($scope) {
-    $scope.table = [
-      [1, 2, 3, 4],
-      [2, 3, 4, 3],
-      [3, 4, 3, 2],
-      [4, 3, 2, 1]
-    ];
-  });
-
+  angular.module('demoApp', []);
+  angular.
+    module('demoApp').
+    component('demoComponent', {
+      template:
+        '<table>'+
+          '<tr ng-repeat="row in table track by $index">'+
+            '<td ng-repeat="data in row track by $index">'+
+              '{{data}}'+
+            '</td>'+
+          '</tr>'+
+        '</table>',
+      controller: function DemoController($scope) {
+        $scope.table = [
+          [1, 2, 3, 4],
+          [2, 3, 4, 3],
+          [3, 4, 3, 2],
+          [4, 3, 2, 1]
+        ];
+      }
+    });
 }
