f94d356b1f883c1190a1d49090f40c06421355ad
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-19 16:38:54 -0400

    formatting / explain linkage

--- _documentation/tutorial/web_bundle/demo_5.js
+++ _documentation/tutorial/web_bundle/demo_5.js
@@ -2,12 +2,12 @@ function demo_5($target) {
   function print(string) {
     $target.append(string + '<br>');
   }
-  var sequence = Array.from(Array(200).keys()).map(function(x){return (x-100)/50;});
+  var sequence = Array.from(Array(200).keys())
+      .map(function(x){return (x-100)/50;});
   var matrix = Caleydo.d3.parser.parseMatrix(
-      // times table
       sequence.map(function (m) {
         return sequence.map(function (n) {
-          // Gentle gradient across the whole thing..
+          // Gentle gradient across the whole thing...
           // with ripples in the middle.
           return m*n + Math.sin(1/(m*m+n*n));
         })
@@ -16,23 +16,26 @@ function demo_5($target) {
       sequence // col_ids
   );
 
+  var create = Caleydo.core.multiform.create;
+  var parse = Caleydo.core.range.parse;
+
   print('Big picture');
-  Caleydo.core.multiform.create(
-      matrix.view(Caleydo.core.range.parse('(0:200:5),(0:200:5)')),
+  create(
+      matrix.view(parse('(0:200:5),(0:200:5)')),
       $target[0],
       {initialVis: 'caleydo-vis-heatmap'}
   );
 
   print('Zoom in');
-  Caleydo.core.multiform.create(
-      matrix.view(Caleydo.core.range.parse('(100:120),(100:120)')),
+  create(
+      matrix.view(parse('(100:120),(100:120)')),
       $target[0],
       {initialVis: 'caleydo-vis-heatmap'}
   );
 
   print('Best of both worlds');
-  Caleydo.core.multiform.create(
-      matrix.view(Caleydo.core.range.parse('(0:200:10,100:120),(0:200:10,100:120)')),
+  create(
+      matrix.view(parse('(0:200:10,100:120),(0:200:10,100:120)')),
       $target[0],
       {initialVis: 'caleydo-vis-heatmap'}
   );
