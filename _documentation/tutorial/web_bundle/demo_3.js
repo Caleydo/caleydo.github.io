c190eeeb74b5355391b67ba7f323cf69b421c52d
mccalluc <mccalluc@users.noreply.github.com>
2016-09-19 14:12:57 -0400

    Mccalluc/weather data (#137)
    * replace parabola with boston weather data
    
    * Date id
    
    * resize iframes
    
    * narrower iframe chrome
    
    * Good enough for now
    I think it would be better to set initial axes, but that does not seem to be an option currently.

--- _documentation/tutorial/web_bundle/demo_3.js
+++ _documentation/tutorial/web_bundle/demo_3.js
@@ -1,20 +1,15 @@
 function demo_3($target) {
   $target.append('<div class="visual">');
-  $target.append('<div class="toolbar">');
 
   Caleydo.d3.parser.parseRemoteMatrix( // Download data
-      'data.csv'
+      'boston-weather.csv'
   ).then(function (matrix) {
     var multiform = Caleydo.core.multiform;
 
     var visual = multiform.create(
         matrix,
         $('.visual')[0],
-        {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
-    );
-    multiform.addSelectVisChooser( // Let user pick visualization
-        $('.toolbar')[0],
-        visual
+        {initialVis: 'scatterplot'} // Specify visualization
     );
   });
 }
--- _documentation/tutorial/web_bundle/demo_4.js
+++ _documentation/tutorial/web_bundle/demo_4.js
@@ -1,28 +1,24 @@
   function demo_4($target) {
     $target.append('<div class="visual">');
-    $target.append('<div class="toolbar">');
     $target.append('<div class="visual">');
-    $target.append('<div class="toolbar">');
+    $target.append('<div class="visual">');
 
     Caleydo.d3.parser.parseRemoteMatrix( // Download data
-        '/documentation/tutorial/web_bundle/data.csv'
+        'boston-weather.csv'
     ).then(function (matrix) {
-          var multiform = Caleydo.core.multiform;
+      var multiform = Caleydo.core.multiform;
 
-          function visualize(index) { // Make a function so we can have more than one
-            var visual = multiform.create(
-                matrix,
-                $target.find('.visual')[index],
-                {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
-            );
-            multiform.addSelectVisChooser( // Let user pick visualization
-                $target.find('.toolbar')[index],
-                visual
-            );
-          }
+      function visualize(index, config) { // Make a function so we can have more than one
+        return(multiform.create(
+            matrix,
+            $target.find('.visual')[index],
+            config
+        ));
+      }
 
-          visualize(0);
-          visualize(1);
-        }
-    )
+      visualize(0, {initialVis: 'scatterplot'});
+      visualize(1, {initialVis: 'scatterplot'});
+      var $table = $(visualize(2, {initialVis: 'table'}).content);
+      $table.height(100).css('overflow-y', 'scroll');
+    })
   }
