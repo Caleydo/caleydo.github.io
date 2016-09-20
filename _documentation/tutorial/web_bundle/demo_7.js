7dc61e70832bfb6fc86427973f377a0b38a13d18
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-20 14:03:31 -0400

    table update -> heatmap

--- _documentation/tutorial/web_bundle/demo_7.js
+++ _documentation/tutorial/web_bundle/demo_7.js
@@ -1,16 +1,19 @@
-function demo_7($target) {
+function demo_7() {
   var table = [
-          ['1', '2', '3', '4'],
-          ['2', '3', '4', '3'],
-          ['3', '4', '3', '2'],
-          ['4', '3', '2', '1']
-        ];
-  var matrix = Caleydo.d3.parser.parseMatrix(
-    table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
-  );
-  Caleydo.core.multiform.create(
-    matrix, $target[0], {initialVis: 'caleydo-vis-heatmap'}
-  );
+    ['1', '2', '3', '4'],
+    ['2', '3', '4', '3'],
+    ['3', '4', '3', '2'],
+    ['4', '3', '2', '1']
+  ];
+  function draw_caleydo(table) {
+    var matrix = Caleydo.d3.parser.parseMatrix(
+      table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
+    );
+    return Caleydo.core.multiform.create(
+      matrix, $('#caleydo')[0], {initialVis: 'caleydo-vis-heatmap'}
+    );
+  }
+  var vis = draw_caleydo(table);
 
   angular.module('demoApp', []);
   angular.
@@ -22,15 +25,16 @@ function demo_7($target) {
             '<td ng-repeat="item in row track by $index">'+
               '<input type="text" size="2" '+
                      'ng-model="table[$parent.$index][$index]" '+
-                     'ng-change="on_change()">'+
+                     'ng-change="on_change($parent.$index, $index)">'+
             '</td>'+
           '</tr>'+
         '</table>'+
         '<code>{{table | json}}</code>',
       controller: function DemoController($scope) {
         $scope.table = table;
-        $scope.on_change = function() {
-          console.log('change!')
+        $scope.on_change = function(x, y) {
+          vis.destroy();
+          vis = draw_caleydo(table);
         }
       }
     });
