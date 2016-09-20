7e1895b2844026b644fd7498257e17daa16e33bd
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-20 09:43:10 -0400

    Working angular matrix example

--- _documentation/tutorial/web_bundle/demo_7.js
+++ _documentation/tutorial/web_bundle/demo_7.js
@@ -7,16 +7,17 @@ function demo_7() {
         '<table>'+
           '<tr ng-repeat="row in table track by $index">'+
             '<td ng-repeat="data in row track by $index">'+
-              '{{data}}'+
+              '<input type="text" ng-model="table[$parent.$index][$index]" size="2">'+
             '</td>'+
           '</tr>'+
-        '</table>',
+        '</table>'+
+        '<code>{{table | json}}</code>',
       controller: function DemoController($scope) {
         $scope.table = [
-          [1, 2, 3, 4],
-          [2, 3, 4, 3],
-          [3, 4, 3, 2],
-          [4, 3, 2, 1]
+          ['1', '2', '3', '4'],
+          ['2', '3', '4', '3'],
+          ['3', '4', '3', '2'],
+          ['4', '3', '2', '1']
         ];
       }
     });
