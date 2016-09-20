6fc4e9674ca24a6cac7d07eb82b57f0877febe92
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-20 14:26:23 -0400

    Add a stripped down angular-only version

--- _documentation/tutorial/web_bundle/demo_8.js
+++ _documentation/tutorial/web_bundle/demo_8.js
@@ -5,6 +5,15 @@ function demo_7() {
     ['3', '4', '3', '2'],
     ['4', '3', '2', '1']
   ];
+  function draw_heatmap(table) {
+    var matrix = Caleydo.d3.parser.parseMatrix(
+      table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
+    );
+    return Caleydo.core.multiform.create(
+      matrix, $('#caleydo')[0], {initialVis: 'caleydo-vis-heatmap'}
+    );
+  }
+  var heatmap = draw_heatmap(table);
 
   angular.module('demoApp', []);
   angular.
@@ -15,13 +24,18 @@ function demo_7() {
           '<tr ng-repeat="row in table track by $index">'+
             '<td ng-repeat="item in row track by $index">'+
               '<input type="text" size="2" '+
-                     'ng-model="table[$parent.$index][$index]">'+
+                     'ng-model="table[$parent.$index][$index]" '+
+                     'ng-change="on_change()">'+
             '</td>'+
           '</tr>'+
         '</table>'+
         '<code>{{table | json}}</code>',
       controller: function DemoController($scope) {
         $scope.table = table;
+        $scope.on_change = function() {
+          heatmap.destroy();
+          heatmap = draw_heatmap(table);
+        }
       }
     });
 }
