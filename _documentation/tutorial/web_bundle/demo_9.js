ff910e866c935bbad549b5d5ad7f92b11d3cd3d0
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-21 15:29:06 -0400

    demo_9 is good, need to go back and clean up the older ones

--- _documentation/tutorial/web_bundle/demo_9.js
+++ _documentation/tutorial/web_bundle/demo_9.js
@@ -7,16 +7,31 @@ function demo_9() {
         ['3', '4', '3', '2'],
         ['4', '3', '2', '1']
       ];
+      $scope.heatmap = false; // Will be replaced with Caleydo object.
     }])
     .directive('caleydoHeatmap', function() {
-      // TODO: update with every change to table.
       return {
         link: function(scope, element, attrs) {
-          var matrix = Caleydo.d3.parser.parseMatrix(
-            scope.table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
-          );
-          Caleydo.core.multiform.create(
-            matrix, element[0], {initialVis: 'caleydo-vis-heatmap'}
+          function redraw() {
+            if (scope.heatmap) {
+              scope.heatmap.destroy();
+            }
+            var matrix = Caleydo.d3.parser.parseMatrix(
+              scope.table,
+              Array(scope.table.length), // Blank arrays as placeholders
+              Array(scope.table[0].length) // for rows and columns
+            );
+            scope.heatmap = Caleydo.core.multiform.create(
+              matrix, element[0], {initialVis: 'caleydo-vis-heatmap'}
+            );
+          }
+          redraw();
+          scope.$watch(
+              'table',
+              function(table) {
+                redraw();
+              },
+              true // change detection based on value rather than reference
           );
         }
       }
@@ -24,7 +39,7 @@ function demo_9() {
     .directive('caleydoEditor', function() {
       return {
         template:
-          'TABLE?:<table>'+
+          '<table>'+
             '<tr ng-repeat="row in table track by $index">'+
               '<td ng-repeat="item in row track by $index">'+
                 '<input type="text" size="2" '+
