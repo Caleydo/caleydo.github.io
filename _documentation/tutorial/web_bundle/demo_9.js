3936b5e2d04afdd3d1560c7af5c2fc4e09fe3170
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-21 14:39:44 -0400

    Checkpoint: heatmap renders, but does not update

--- _documentation/tutorial/web_bundle/demo_9.js
+++ _documentation/tutorial/web_bundle/demo_9.js
@@ -1,41 +1,38 @@
-function demo_8() {
-  var table = [
-    ['1', '2', '3', '4'],
-    ['2', '3', '4', '3'],
-    ['3', '4', '3', '2'],
-    ['4', '3', '2', '1']
-  ];
-  function draw_heatmap(table) { // Function for rendering a heatmap
-    var matrix = Caleydo.d3.parser.parseMatrix(
-      table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
-    );
-    return Caleydo.core.multiform.create(
-      matrix, $('[caledo-heatmap]')[0], {initialVis: 'caleydo-vis-heatmap'}
-    );
-  }
-  var heatmap = draw_heatmap(table); // Use it on page load
-
-  angular.module('demoApp', []);
-  angular.
-    module('demoApp').
-    component('demoComponent', {
-      template:
-        '<table>'+
-          '<tr ng-repeat="row in table track by $index">'+
-            '<td ng-repeat="item in row track by $index">'+
-              '<input type="text" size="2" '+
-                     'ng-model="table[$parent.$index][$index]" '+
-                     'ng-change="on_change()">'+ // Watch for changes...
-            '</td>'+
-          '</tr>'+
-        '</table>'+
-        '<code>{'+'{table | json}}</code>',
-      controller: function DemoController($scope) {
-        $scope.table = table;
-        $scope.on_change = function() { // ... and update the UI with each change.
-          heatmap.destroy();
-          heatmap = draw_heatmap(table);
+function demo_9() {
+  angular.module('demoApp', [])
+    .controller('DemoController', ['$scope', function($scope) {
+      $scope.table = [
+        ['1', '2', '3', '4'],
+        ['2', '3', '4', '3'],
+        ['3', '4', '3', '2'],
+        ['4', '3', '2', '1']
+      ];
+    }])
+    .directive('caleydoHeatmap', function() {
+      // TODO: update with every change to table.
+      return {
+        link: function(scope, element, attrs) {
+          var matrix = Caleydo.d3.parser.parseMatrix(
+            scope.table, [1, 2, 3, 4], ['A', 'B', 'C', 'D']
+          );
+          Caleydo.core.multiform.create(
+            matrix, element[0], {initialVis: 'caleydo-vis-heatmap'}
+          );
         }
       }
+    })
+    .directive('caleydoEditor', function() {
+      return {
+        template:
+          'TABLE?:<table>'+
+            '<tr ng-repeat="row in table track by $index">'+
+              '<td ng-repeat="item in row track by $index">'+
+                '<input type="text" size="2" '+
+                       'ng-model="table[$parent.$index][$index]">'+
+              '</td>'+
+            '</tr>'+
+          '</table>'+
+          '<code>{'+'{table | json}}</code>'
+      };
     });
 }
