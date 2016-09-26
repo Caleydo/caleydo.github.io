09e44bdfc998471a247f4ca9182a94a5d00feee5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-26 17:44:49 -0400

    Checkpoint: Not confident that all of this works

---
+++ _documentation/tutorial/web_bundle/demo_0/demo_function.js
@@ -0,0 +1,7 @@
+---
+# This YML block means Jekyll will apply Liquid templates.
+# Change the name below to match parent directory.
+---
+function demo_0($target) {
+  {% include_relative demo.js %}
+}
