8c0f2ec708c7a5c44e309fdb87929b6f9d5387df
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-27 17:50:23 -0400

    Visualization shows up in list (Does not do anything)

--- _documentation/tutorial/web_bundle/demo_vis.js
+++ _documentation/tutorial/web_bundle/demo_vis.js
@@ -1,6 +1,32 @@
 function demo_vis() {
   // Use the AMD; This would usually be at the top level and include other dependencies.
   require(['demo_vis_amd', "require", "exports"], function (demo_vis, require, exports) {
-    demo_vis.hello();
+    Caleydo.core.plugin.push(demo_vis);
+    var n = Caleydo.core.plugin.list().length;
+    console.log(Caleydo.core.plugin.list()[n-1]);
+
+    $('body').append('<div class="visual">');
+    $('body').append('<div class="toolbar">');
+
+    var matrix = Caleydo.d3.parser.parseMatrix(
+      [ // raw data as 2D array
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1]
+      ],
+      [1, 2, 3], // row_ids
+      ['A', 'B', 'C'] // col_ids
+    );
+    var multiform = Caleydo.core.multiform;
+
+    var visual = multiform.create(
+      matrix,
+      $('body').find('.visual')[0],
+      {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
+    );
+    multiform.addSelectVisChooser( // Let user pick visualization
+      $('body').find('.toolbar')[0],
+      visual
+    );
   });
 }
--- _documentation/tutorial/web_bundle/demo_vis_amd.js
+++ _documentation/tutorial/web_bundle/demo_vis_amd.js
@@ -2,4 +2,7 @@ define(["require", "exports"], function (require, exports) {
   exports.hello =  function() {
     console.log('hello world');
   };
+  exports.type = 'vis';
+  exports.name = 'Demo Vis';
+  exports.filter = 'matrix';
 });
