29e7df9dbb39a43e450fd1055af00887d9df02c3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-28 12:05:50 -0400

    AMD is not needed

--- _documentation/tutorial/web_bundle/demo_vis.js
+++ _documentation/tutorial/web_bundle/demo_vis.js
@@ -1,30 +1,39 @@
 function demo_vis() {
-  // Use the AMD; This would usually be at the top level and include other dependencies.
-  require(['demo_vis_amd', "require", "exports"], function (demo_vis, require, exports) {
-    Caleydo.core.plugin.push(demo_vis);
-
-    $('body').append('<div class="visual">');
-    $('body').append('<div class="toolbar">');
-
-    var matrix = Caleydo.d3.parser.parseMatrix(
-      [ // raw data as 2D array
-        [0, 0, 1],
-        [0, 1, 1],
-        [1, 1, 1]
-      ],
-      [1, 2, 3], // row_ids
-      ['A', 'B', 'C'] // col_ids
-    );
-    var multiform = Caleydo.core.multiform;
-
-    var visual = multiform.create(
-      matrix,
-      $('body').find('.visual')[0],
-      {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
-    );
-    multiform.addSelectVisChooser( // Let user pick visualization
-      $('body').find('.toolbar')[0],
-      visual
-    );
+  Caleydo.core.plugin.push({
+    type: 'vis',
+    name: 'Demo Vis',
+    filter: 'matrix',
+    // .loader can also be used, if you need to defer.
+    instance: {
+      create: function (data, parent, options) {
+        console.log('data:', data);
+        console.log('parent:', parent);
+        console.log('options:', options);
+      }
+    }
   });
+
+  $('body').append('<div class="visual">');
+  $('body').append('<div class="toolbar">');
+
+  var matrix = Caleydo.d3.parser.parseMatrix(
+    [ // raw data as 2D array
+      [0, 0, 1],
+      [0, 1, 1],
+      [1, 1, 1]
+    ],
+    [1, 2, 3], // row_ids
+    ['A', 'B', 'C'] // col_ids
+  );
+  var multiform = Caleydo.core.multiform;
+
+  var visual = multiform.create(
+    matrix,
+    $('body').find('.visual')[0],
+    {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
+  );
+  multiform.addSelectVisChooser( // Let user pick visualization
+    $('body').find('.toolbar')[0],
+    visual
+  );
 }
