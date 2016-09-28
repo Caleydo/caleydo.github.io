1e894dbe577c5240e3a91ceacbf43f417903c0bf
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-09-28 12:43:24 -0400

    Checkpoint: nothing on the screen, but all the data is available.

--- _documentation/tutorial/web_bundle/demo_vis.js
+++ _documentation/tutorial/web_bundle/demo_vis.js
@@ -1,39 +1,39 @@
 function demo_vis() {
   Caleydo.core.plugin.push({
-    type: 'vis',
-    name: 'Demo Vis',
-    filter: 'matrix',
-    // .loader can also be used, if you need to defer.
+    id: 'demo-vis',
+    type: 'vis', // The larger Caleydo ecosystem also includes apps and server-side components.
+    name: 'Demo Visualization',
+    filter: 'matrix', // The data type which this visualization supports
     instance: {
       create: function (data, parent, options) {
-        console.log('data:', data);
-        console.log('parent:', parent);
-        console.log('options:', options);
+        data.loader.data({}, Caleydo.core.range.parse('0:-1,0:-1'))
+            .then(
+              function(value) {
+                console.log('data:', data);
+                console.log('parent:', parent);
+                console.log('options:', options);
+                console.log('matrix:',value);
+              },
+              function(reason) {
+                console.log('error:',reason);
+              }
+            );
       }
     }
   });
 
-  $('body').append('<div class="visual">');
-  $('body').append('<div class="toolbar">');
-
   var matrix = Caleydo.d3.parser.parseMatrix(
     [ // raw data as 2D array
-      [0, 0, 1],
-      [0, 1, 1],
-      [1, 1, 1]
+      [0, 1, 2],
+      [1, 2, 3],
+      [2, 3, 4]
     ],
     [1, 2, 3], // row_ids
     ['A', 'B', 'C'] // col_ids
   );
-  var multiform = Caleydo.core.multiform;
-
-  var visual = multiform.create(
+  Caleydo.core.multiform.create(
     matrix,
-    $('body').find('.visual')[0],
-    {initialVis: 'caleydo-vis-heatmap'} // Specify visualization
-  );
-  multiform.addSelectVisChooser( // Let user pick visualization
-    $('body').find('.toolbar')[0],
-    visual
+    $('body')[0],
+    {initialVis: 'demo-vis'} // Specify visualization
   );
 }
